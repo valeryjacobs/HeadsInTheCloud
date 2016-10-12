2045ce8acca8af9b462bf760313dc18057803b56
tferrand <tferrand@TESSLENOVO>
2016-10-12 17:05:17 +0200

    More on the views

--- HeadsInTheCloud/HeadsInTheCloud/Views/SelfiesPage.xaml.cs
+++ HeadsInTheCloud/HeadsInTheCloud/Views/SelfiesPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Plugin.Media;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,12 @@ namespace HeadsInTheCloud.Views
         public SelfiesPage()
         {
             InitializeComponent();
+        }
+
+        void OnTakePicture(object sender, EventArgs e)
+        {
+            CrossMedia.Current.Initialize();
+            CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { DefaultCamera = Plugin.Media.Abstractions.CameraDevice.Front });
         }
     }
 }
